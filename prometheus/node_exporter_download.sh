68d4a445cbebc33b1016531004728d811978cc80
zhenkai.sun <zhenkai.sun@qq.com>
2023-07-18 03:04:46 +0000

    fix

--- prometheus/node_exporter_download.sh
+++ prometheus/node_exporter_download.sh
@@ -1,7 +1,7 @@
 DEST_FILE=/tmp/prometheus_node_exporter.tar.gz
 DEST_INSTALL=/data/app
-VERSION=v1.6.1
-SRC=https://github.com/prometheus/node_exporter/releases/download/$VERSION/node_exporter-$VERSION.linux-amd64.tar.gz
+VERSION=1.6.1
+SRC=https://github.com/prometheus/node_exporter/releases/download/v$VERSION/node_exporter-$VERSION.linux-amd64.tar.gz
 
 wget $SRC -O $DEST_FILE
 [ ! -e $DEST_INSTALL ] && mkdir $DEST_INSTALL
