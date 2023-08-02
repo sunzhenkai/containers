4015585e9c9ff7d396da590626eb3362f2436d02
zhenkai.sun <zhenkai.sun@qq.com>
2023-08-02 12:33:59 +0000

    mod

--- hadoop/run.sh
+++ hadoop/run.sh
@@ -6,11 +6,11 @@ CUR_DATA_DIR=$DATA_DIR/$(basename $BASE)
 COMPOSE_FILE=$BASE/docker-compose.yaml
 
 function create() {
-    docker-compose up -d -f $COMPOSE_FILE
+    docker compose up -d -f $COMPOSE_FILE
 }
 
 function down() {
-    docker-compose down -f $COMPOSE_FILE
+    docker compose down -f $COMPOSE_FILE
 }
 
 if [ "$1" == "" ]; then
--- hadoop/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "hadoop test script"
