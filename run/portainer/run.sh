405d3d71428c53a0f3786c03d20de3fa22000365
zhenkai.sun <zhenkai.sun@qq.com>
2024-11-07 05:47:08 +0000

    update

--- run/portainer/run.sh
+++ run/portainer/run.sh
@@ -3,4 +3,4 @@ docker run -d -p 3014:8000 -p 3015:9000 -p 3016:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /data/docker/portainer:/data \
-    192.168.6.7:5000/portainer/portainer-ce:latest
+    portainer/portainer-ce:latest
